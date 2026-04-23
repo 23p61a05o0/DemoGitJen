8093730d5b8cf7d287f688ff499398a9c86fec7e
23p61a05o0 <23p61a05o0@vbithyd.ac.in>
2026-04-23 21:47:13 +0530

    Created.... Back.java

---
+++ Back.java
@@ -0,0 +1,26 @@
+public class Back {
+    // Fields (Attributes)
+    private String status;
+
+    // Constructor
+    public Back(String status) {
+        this.status = status;
+    }
+
+    // Method to display a message
+    public void displayStatus() {
+        System.out.println("Current status is: " + status);
+    }
+
+    // Main method: The entry point of the program
+    public static void main(String[] args) {
+        // Creating an instance of the Back class
+        Back myBack = new Back("Active");
+        
+        // Calling a method on the object
+        myBack.displayStatus();
+        
+        // Example of printing the class name using reflection
+        System.out.println("Class Name: " + myBack.getClass().getSimpleName());
+    }
+}
